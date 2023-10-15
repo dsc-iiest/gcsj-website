2b2f970563cf68798633faafc4faaf4e7c758530
Abhijit <artistrup07@gmail.com>
2023-10-15 11:10:52 +0530

    correcting banner pic alignments

--- src/components/LeaderB.js
+++ src/components/LeaderB.js
@@ -5,29 +5,32 @@ import ScrollToContent from "./scrollBanner";
 const Report = () => {
     return (
         <>
-            <Box className="Lbanner" sx = {{marginTop: "60px"}}>
-                <Typography
-                    sx={{
-                        fontWeight: 600,
-                        padding: "2rem",
-                        fontSize: "min(12vw, 4rem)",
-                    }}
-                >
-                    LeaderBoard
-                </Typography>
-                <Typography
-                    variant="h6"
-                    sx={{
-                        textAlign: "center",
-                        minWidth: "28ch",
-                        maxWidth: "70ch",
-                        color: "hsla(0, 0%, 100%, 0.65)",
-                    }}
-                >
-                    Discover the top performers and track your progress with our
-                    leaderboard. Join the competition and climb the ranks to
-                    showcase your skills!
-                </Typography>
+            <Box className="Lbanner" sx={{ marginTop: "60px" }}>
+                <Box>
+                    <Typography
+                        sx={{
+                            textAlign: "center",
+                            fontWeight: 600,
+                            fontSize: "min(12vw, 4rem)",
+                            marginBottom: "40px"
+                        }}
+                    >
+                        LeaderBoard
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            textAlign: "center",
+                            minWidth: "28ch",
+                            maxWidth: "70ch",
+                            color: "hsla(0, 0%, 100%, 0.65)",
+                        }}
+                    >
+                        Discover the top performers and track your progress with
+                        our leaderboard. Join the competition and climb the
+                        ranks to showcase your skills!
+                    </Typography>
+                </Box>
                 <ScrollToContent />
             </Box>
             <LeaderBcontent />
